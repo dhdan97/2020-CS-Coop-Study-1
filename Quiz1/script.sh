415406972eb3adb567e28d415f5fb71c9255c02f
Daniel Hernandez <dhdan97@gmail.com>
2020-06-10 23:17:42 -0400

    Fixed script for Question2

--- Quiz1/script.sh
+++ Quiz1/script.sh
@@ -5,16 +5,16 @@ rm *.bak
 
 for file in `ls *.py`
 do
-	lines  = `wc -l < $file`
+	lines=`wc -l < $file`
 	echo $file
 
-	if [$lines -ge 10 ]
+	if [ $lines -ge 10 ]
 	then
-		long_name = "long_"$file
+		long_name="long_"$file
 		echo "greater than or equal to 10"
 		`mv $file $long_name`
 	else
-		short_name = "short_"$file
+		short_name="short_"$file
 		echo "less than 10"
 		`mv $file $short_name`
 	fi
